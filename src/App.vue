3551b41b2a74b3dfeda33cb2244b47046344622e
Gorbach Dmitry <gdv@maxil.org>
2024-02-20 13:29:26 +0300

    final tuning stylelint! work in all files!

--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,34 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="/vite.svg" class="logo" alt="Vite logo">
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
+
+      <img
+        src="./assets/vue.svg" class="logo vue" alt="Vue logo"
+      >
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style scoped lang="scss">
   .logo {
-    color: #444;
-    will-change: filter;
-    transition: filter 300ms;
     height: 6em;
     padding: 1.5em;
+    color: #444;
+    transition: filter 300ms;
+    will-change: filter;
+
+    .test {
+      background-color: #000;
+      color: #555;
+      font-size: 16px;
+    }
   }
+
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
   }
+
   .logo.vue:hover {
     filter: drop-shadow(0 0 2em #42b883aa);
   }
